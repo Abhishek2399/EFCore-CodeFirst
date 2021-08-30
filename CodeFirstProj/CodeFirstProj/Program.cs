d4cdef5812cca2d2791cf98b27ac44fe672116fc
Abhishek <abhibhovar@gmail.com>
2021-08-30 09:45:42 +0530

    EFCore - CodeFirst

---
+++ CodeFirstProj/CodeFirstProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeFirstProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
