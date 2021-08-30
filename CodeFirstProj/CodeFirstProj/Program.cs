3af24a034f04ea3ab28ec99373df1f5cd5f88784
Abhishek <abhibhovar@gmail.com>
2021-08-30 10:18:33 +0530

    Modified

--- CodeFirstProj/CodeFirstProj/Program.cs
+++ CodeFirstProj/CodeFirstProj/Program.cs
@@ -3,7 +3,9 @@
 namespace CodeFirstProj
 {
     class Program
-    {
+    { 
+        // Seperate folder should be created to organise all classes "Models" named is preferred
+        // 1-Class) -> DbContext , based on Proj name we should write eg ProjDbContext 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
